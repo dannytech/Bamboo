edc9b42be1889718f64ef63f2be09dfebbe3effe
Daniel Hellstern <dhellstern@outlook.com>
2020-03-22 02:41:54 -0700

    Added player object

---
+++ Bamboo/Game/BambooPlayer.cs
@@ -0,0 +1,17 @@
+﻿using Bamboo.Server;
+using System;
+
+namespace Bamboo.Game
+{
+    class BambooPlayer
+    {
+        public string Username;
+        public Guid UUID;
+
+        public BambooPlayer(string username, Guid uuid)
+        {
+            Username = username;
+            UUID = uuid;
+        }
+    }
+}
